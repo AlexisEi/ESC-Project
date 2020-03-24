92716d09becdb8f7eea29b62228b2f01a824619b
Ong Li-Chang <onglichang@hotmail.com>
2020-03-24 11:10:40 +0800

    Update connect.php

--- connect.php
+++ connect.php
@@ -2,11 +2,11 @@
     $skilltags_required = $_POST['skilltags_required'];
     
     //DB Connection
-    $conn = new mysqli('192.168.64.2:3307', 'root', 'root', 'BSOS_DB');
+    $conn = mysqli_connect('localhost', 'chang', 'helloworld1!', 'BSOS_DB');
     if($conn->connect_error){
         die('Connection Failed : '.$conn->connect_error);
     }else{
-        $stmt = $conn->prepare("insert into customer_queries(skilltags_required)
+        $stmt = $conn->prepare("INSERT into customer_queries(skilltags_required)
             values(?)");
         $stmt->bind_param("s", $skilltags_required);
         $stmt->execute();
